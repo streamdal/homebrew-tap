db8b448fd436de828ab57ded89cb8ddc1b791f63
Mark Gregan <mark@batch.sh>
2021-04-16 17:55:08 -0400

    v0.22.0

--- plumber.rb
+++ plumber.rb
@@ -4,14 +4,14 @@ class Plumber < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/batchcorp/plumber/releases/download/v0.21.0/plumber-darwin"
-    sha256 "ebc1280a0cf920304fe5371ac5b7d025fd04fc3b40e0dc9f531fa424957179d7"
+    url "https://github.com/batchcorp/plumber/releases/download/v0.22.0/plumber-darwin"
+    sha256 "64a6dca441d1362c346d3911b38524c898327762b1a8deca67e3daa29cd26fb3"
   elsif OS.linux?
 
     case RbConfig::CONFIG["host_cpu"]
     when "x86_64"
-      url "https://github.com/batchcorp/plumber/releases/download/v0.21.0/plumber-linux"
-      sha256 "441bc47351d23b1e4c4caf054179a78f6e2dd7d2f03e432045890dd4153b2032"
+      url "https://github.com/batchcorp/plumber/releases/download/v0.22.0/plumber-linux"
+      sha256 "e096542433c63d585242e0aa77390051f51a98ed410f2fb352a5ae36ff8a48c9"
     else
       ohdie "Architecture not supported by this formula"
     end
