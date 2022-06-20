448c5cecc4a8ad06c8a3b3b0f4cc11ecd4c79d08
Daniel Selans <daniel.selans@gmail.com>
2022-06-20 14:12:02 -0700

    updated to support arm

--- plumber.rb
+++ plumber.rb
@@ -3,39 +3,36 @@ class Plumber < Formula
   homepage "https://github.com/batchcorp/plumber"
   license "MIT"
 
-  if OS.mac?
-    case RbConfig::CONFIG["host_cpu"]
-    when "arm64"
+  on_macos do
+    if Hardware::CPU.arm?
       url "https://github.com/batchcorp/plumber/releases/download/v1.9.0/plumber-darwin-arm64"
       sha256 "136ff4aa3a07cef84e183f4c8e2c547b1410a60c6309aa2e7c1fe3256223ba93"
     else
       url "https://github.com/batchcorp/plumber/releases/download/v1.9.0/plumber-darwin"
       sha256 "c6e8ca339bf9f746dbf7569869a1357e4ef24da1818fb8dc8e56f61b97121166"
     end
-  elsif OS.linux?
-    case RbConfig::CONFIG["host_cpu"]
-    when "x86_64"
+  end
+
+  on_linux do
+    if Hardware::CPU::intel?
       url "https://github.com/batchcorp/plumber/releases/download/v1.9.0/plumber-linux"
       sha256 "8dec781b3ba8da159f0eb88f67d596ef8e92e8abac33a1dde8abbe8fa8e8f9bb"
     else
-      ohdie "Architecture not supported by this formula"
+      ohdie "Unsupported CPU architecture"
     end
-  else
-    ohdie "Unsupported OS"
   end
 
   def install
-    if OS.mac?
-      case RbConfig::CONFIG["host_cpu"]
-      when "arm64"
+    on_macos do
+      if Hardware::CPU.arm?
         bin.install "plumber-darwin-arm64" => "plumber"
       else
         bin.install "plumber-darwin" => "plumber"
       end
-    elsif OS.linux?
+    end
+
+    on_linux do
       bin.install "plumber-linux" => "plumber"
-    else
-      ohdie "Unsupported OS"
     end
   end
 
