fb2d2a8b717bcf6c7eee7a71ed6744fffa0a704f
Mark G <mark@gregan.org>
2023-07-12 12:31:59 -0700

    v2.5.1

--- plumber.rb
+++ plumber.rb
@@ -5,18 +5,18 @@ class Plumber < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/streamdal/plumber/releases/download/v2.5.0/plumber-darwin-arm64"
-      sha256 "53785ac7d4e9d40238e7224bbaa0461715f38f830fa7bc6bb582f81431dc55cb"
+      url "https://github.com/streamdal/plumber/releases/download/v2.5.1/plumber-darwin-arm64"
+      sha256 "fffb5f6a2c62821c55f66f68c52178924758f20629c16e1290daed18fde49a0c"
     else
-      url "https://github.com/streamdal/plumber/releases/download/v2.5.0/plumber-darwin"
-      sha256 "0eee4547663293218a4b43d5e5f815a3a07574af2869d8880b05753e3434fcda"
+      url "https://github.com/streamdal/plumber/releases/download/v2.5.1/plumber-darwin"
+      sha256 "a9dc41503496e47ba4007f15085f5fd9c5435bb73ed07cc38fb9d8c1ef0a9c61"
     end
   end
 
   on_linux do
     if Hardware::CPU::intel?
-      url "https://github.com/streamdal/plumber/releases/download/v2.5.0/plumber-linux"
-      sha256 "f916e83d93517bf3d24bbbd6498aa85b60c6cbdfc809525d2b30ef850c685dda"
+      url "https://github.com/streamdal/plumber/releases/download/v2.5.1/plumber-linux"
+      sha256 "e11dd1f4e4f81f31f8c3f962f069cccf27e6dd8066ffc6f320bbf994be1ac8b2"
     else
       ohdie "Unsupported CPU architecture"
     end
